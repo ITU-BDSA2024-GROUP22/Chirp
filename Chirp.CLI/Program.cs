52ffbafc59617a01905280c75a25e47244f4ba56
Olivia-Brophy <brop@itu.dk>
2024-08-28 16:16:07 +0200

    Created Chirp, uploaded the CSV file and wrote test code

---
+++ Chirp.CLI/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+try
+{
+    using StreamReader reader = new("chirp_cli_db.csv");
+    string text = reader.ReadToEnd();
+    Console.WriteLine(text);
+}
+catch (IOException e)
+{
+    Console.WriteLine("The file could not be read");
+    Console.WriteLine(e.Message);
+}
+
+/*
+List<string> cheeps = new() { "Hello, BDSA students!", "Welcome to the course!", "I hope you had a good summer." };
+
+foreach (var cheep in cheeps)
+{
+    Console.WriteLine(cheep);
+    Thread.Sleep(1000);
+}
+*/
