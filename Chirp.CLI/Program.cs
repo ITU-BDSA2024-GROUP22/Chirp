2d15fdd4e0cbbb704b855ba7bde71884f7427f68
AVNBuelow <alby@itu.dk>
2024-09-03 15:37:53 +0200

    the read option now works. Co-Authors: anyp, brop, stik, kamp

--- Chirp.CLI/Program.cs
+++ Chirp.CLI/Program.cs
@@ -1,15 +1,51 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-try
+using System.Text.RegularExpressions;
+
+class Program
 {
-    using StreamReader reader = new("chirp_cli_db.csv");
-    string text = reader.ReadToEnd();
-    Console.WriteLine(text);
-}
-catch (IOException e)
-{
-    Console.WriteLine("The file could not be read");
-    Console.WriteLine(e.Message);
+    static void Main(string[] args)
+    {
+        
+        try
+        {
+            using StreamReader reader = new("chirp_cli_db.csv");
+            string text = reader.ReadLine();
+
+            while (!reader.EndOfStream)
+            {
+                text = reader.ReadLine();
+                
+                int firstIndex = text.IndexOf(',');
+                int lastIndex = text.LastIndexOf(',');
+                
+                string author = text.Substring(0,firstIndex);
+                string message = text.Substring(firstIndex + 1, lastIndex - firstIndex - 1).TrimStart('\"').TrimEnd('\"');
+                int date = int.Parse(text.Substring(lastIndex + 1));
+
+                string output = (author + " @ " + dateConverter(date) + ": " + message);
+                
+                Console.WriteLine(output);
+            }
+            
+        }
+        catch (IOException e)
+        {
+            Console.WriteLine("The file could not be read");
+            Console.WriteLine(e.Message);
+        }
+    }
+
+    static string dateConverter(int unix)
+    {
+        DateTimeOffset dto = DateTimeOffset.FromUnixTimeSeconds(unix + 3600);
+        return dto.ToString();
+    }
+
+    static string toCSV()
+    {
+        string user = Environment.UserName;
+    }
 }
 
 /*
