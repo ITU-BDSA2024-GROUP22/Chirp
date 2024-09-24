92481c7a2ede84819ab1363e96dc449a01f06711
karokamp <kamp@itu.dk>
2024-09-24 12:28:06 +0200

    refactored
    Co-authored-by: Anna <anyp@itu.dk>
    Co-authored-by: Stina <stik@itu.dk>
    Co-authored-by: Olivia <brop@itu.dk>
    Co-authored-by: Alberte <alby@itu.dk> Co-authored-by Helge, Lecture4 notes

--- src/Chirp.CLI/Program.cs
+++ src/Chirp.CLI/Program.cs
@@ -1,11 +1,44 @@
 ﻿using Chirp.CLI;
 using DocoptNet;
 using SimpleDB;
+using System.Net;
+using System.Net.Http.Headers;
+using System.Net.Http.Json;
 
 public class Program
 {
-    public static void Main(string[] args)
+    public static async Task Main(string[] args)
     {
+        
+        //Helge lecture4-notes
+        
+        var baseURL = "https://bdsagroup22chirpremotedb.azurewebsites.net/";
+        using HttpClient client = new();
+        client.BaseAddress = new Uri(baseURL);
+
+        // Sequential execution
+        var watch = System.Diagnostics.Stopwatch.StartNew();
+        // first HTTP request
+        var fstResponse = await client.GetAsync("/");
+        // second HTTP request
+        var sndResponse = await client.GetAsync("/");
+        watch.Stop();
+
+        Console.WriteLine($"Sequential HTTP requests ... done after {watch.ElapsedMilliseconds}ms");
+
+        // Concurrent execution
+        watch = System.Diagnostics.Stopwatch.StartNew();
+        // first HTTP request
+        var fstRequestTask = client.GetAsync("/");
+        // second HTTP request
+        var sndRequestTask = client.GetAsync("/");
+
+        var thrdResponse = await fstRequestTask;
+        var frthResponse = await sndRequestTask;
+        watch.Stop();
+
+        Console.WriteLine($"Concurrent HTTP requests ... done after {watch.ElapsedMilliseconds}ms");
+        /*
         //Testing our release workflow with a new branch :)))))
         const string usage = @"Chirp CLI version.
 
@@ -36,6 +69,7 @@ public class Program
             var cheeps = database.Read(3);
             UserInterface.PrintCheeps(cheeps);
         }
+        */
     }
 
     public record Cheep(string Author, string Message, long Timestamp);
