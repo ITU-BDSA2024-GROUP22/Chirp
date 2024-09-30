a6047f0d7b526aac4dea5ce1ccac84de1d70af10
AVNBuelow <alby@itu.dk>
2024-09-30 23:02:16 +0200

    more refactoring, still errors and missing logic
    Co-authored-by: Stina <stik@itu.dk> Co-authored-by: Olivia <brop@itu.dk>

--- src/Chirp.Razor/DBFacade.cs
+++ src/Chirp.Razor/DBFacade.cs
@@ -1,29 +1,31 @@
 using System.Data;
+using Chirp.SQLite;
 using Microsoft.Data.Sqlite;
 
 namespace Chirp.Razor;
 
 public class DBFacade
 {
-    private SqliteConnection connection;
     private const string sqlGetCheeps = "SELECT username, text, pub_date FROM message JOIN user ON user_id = author_id";
+
+    private readonly SqliteConnection connection;
     //private const string sqlGetAuthor = 
 
     public DBFacade()
     {
-        var sqlDBFilePath = System.Environment.GetEnvironmentVariable("CHIRPDBPATH") ?? "/tmp/chirp.db";
-        this.connection =
+        var sqlDBFilePath = Environment.GetEnvironmentVariable("CHIRPDBPATH") ?? "/tmp/chirp.db";
+        connection =
             new SqliteConnection($"Data Source={sqlDBFilePath}"); //vi har nu en connection til en database
     }
 
     public void Open()
     {
-        this.connection.Open();
+        connection.Open();
     }
 
-    public List<CheepViewModel> GetCheeps()
+    public List<Cheep> GetCheeps()
     {
-        var result = new List<CheepViewModel>();
+        var result = new List<Cheep>();
         var command = connection.CreateCommand();
         command.CommandText = sqlGetCheeps;
 
@@ -31,24 +33,24 @@ public class DBFacade
         while (reader.Read())
         {
             var dataRecord = (IDataRecord)reader;
-            result.Add(new CheepViewModel(dataRecord[0].ToString(), dataRecord[1].ToString(), UnixTimeStampToDateTimeString((Int64)dataRecord[2])));
+            result.Add(new Cheep(dataRecord[0].ToString(), dataRecord[1].ToString(),
+                (((long)dataRecord[2])))); // Tjek at den viser unix ordentligt på hjemmeside
         }
 
         return result;
     }
-    
-    public List<CheepViewModel> GetCheepsFromAuthor(string author)
+
+    public List<Cheep> GetCheepsFromAuthor(string author)
     {
-        return new List<CheepViewModel>();
+        return new List<Cheep>();
     }
-    
-    
+
+
     private static string UnixTimeStampToDateTimeString(double unixTimeStamp)
     {
         // Unix timestamp is seconds past epoch
-        DateTime dateTime = new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc);
+        var dateTime = new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc);
         dateTime = dateTime.AddSeconds(unixTimeStamp);
         return dateTime.ToString("MM/dd/yy H:mm:ss");
     }
-    
 }
