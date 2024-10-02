146a0e2e32859a9a430ccc2f56736cbd400235ac
karokamp <kamp@itu.dk>
2024-10-02 19:01:14 +0200

    TestGetAllCheeps works

--- test/Chirp.Tests/ProgramUnitTests.cs
+++ test/Chirp.Tests/ProgramUnitTests.cs
@@ -1,12 +1,17 @@
+using Chirp.Razor;
+
+
 namespace Chirp.Tests;
 
 public class ProgramUnitTests
 {
-    [Fact]
-    public void testMain()
-    {
-    }
+    private DBFacade _dbFacade;
 
+    public ProgramUnitTests()
+    {
+        // Initialiserer DBFacade i konstruktøren
+        _dbFacade = new DBFacade();
+    }
 
     //Test that dateConverter from DBFacade converts timestamp correctly
     [Fact]
@@ -18,6 +23,18 @@ public class ProgramUnitTests
         var result = Razor.DBFacade.UnixTimeStampToDateTimeString(unixTimestamp);
 
         Assert.Equal(expectedTime, result);
+    }
+
+    [Fact]
+    public void TestGetCheeps()
+    {
+        //Først laver jeg en mock database og tilføjer nogle cheeps
+
+        //Antal cheeps i databasen
+        var expectedCheepsInDatabase = 686;
+        var result = _dbFacade.GetAllCheeps();
+
+        Assert.Equal(expectedCheepsInDatabase, result.Count);
     }
 
 }
