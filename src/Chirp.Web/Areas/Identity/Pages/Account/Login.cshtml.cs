20bb443c5455bace6400f6bf7479dcf29ce96e7e
Anna <anyp@itu.dk>
2024-11-03 10:43:26 +0100

    Added name field and Username shown on Chirp instead of mail

--- src/Chirp.Web/Areas/Identity/Pages/Account/Login.cshtml.cs
+++ src/Chirp.Web/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -66,7 +66,6 @@ namespace Chirp.Web.Areas.Identity.Pages.Account
             ///     directly from your code. This API may change or be removed in future releases.
             /// </summary>
             [Required]
-            [EmailAddress]
             public string Email { get; set; }
 
             /// <summary>
--- src/Chirp.Web/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ src/Chirp.Web/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -85,6 +85,14 @@ namespace Chirp.Web.Areas.Identity.Pages.Account
             ///     directly from your code. This API may change or be removed in future releases.
             /// </summary>
             [Required]
+            [Display(Name = "Username")]
+            public string Username { get; set; }
+
+            /// <summary>
+            ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
+            ///     directly from your code. This API may change or be removed in future releases.
+            /// </summary>
+            [Required]
             [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
             [DataType(DataType.Password)]
             [Display(Name = "Password")]
@@ -115,7 +123,7 @@ namespace Chirp.Web.Areas.Identity.Pages.Account
             {
                 var user = CreateUser();
 
-                await _userStore.SetUserNameAsync(user, Input.Email, CancellationToken.None);
+                await _userStore.SetUserNameAsync(user, Input.Username, CancellationToken.None);
                 await _emailStore.SetEmailAsync(user, Input.Email, CancellationToken.None);
                 var result = await _userManager.CreateAsync(user, Input.Password);
 
