a089dd9fc8787b9e6824775b469f873a39ee4a4d
AVNBuelow <alby@itu.dk>
2024-11-13 15:20:45 +0100

    moved logic into usertimeline also. UI works but feature does not

--- src/Chirp.Web/Pages/UserTimeline.cshtml.cs
+++ src/Chirp.Web/Pages/UserTimeline.cshtml.cs
@@ -18,14 +18,31 @@ public class UserTimelineModel : PageModel
     public required Task<List<CheepDTO>> Cheeps { get; set; }
     public required Task<AuthorDTO> Author { get; set; }
     public int CurrentPage { get; set; }
+    public string Text { get; set; }
 
     public ActionResult OnGet([FromQuery] int? page, string author)
     {
-
         this.Author = _service.GetAuthorByName(author);
 
         CurrentPage = page ?? 1;
         Cheeps = _service.GetCheepsFromAuthor(author, CurrentPage);
         return Page();
+    }
+
+    public async Task<IActionResult> OnPostAsync()
+    {
+        if (!User.Identity.IsAuthenticated || string.IsNullOrWhiteSpace(Text))
+        {
+            return Page();
+        }
+
+        var author = await this.Author;
+
+        var postingAuthorName = User.Identity.Name;
+        var postingAuthor = await _service.GetAuthorByName(postingAuthorName);
+
+        await _service.CreateCheep(author, Text, DateTime.UtcNow);
+
+        return RedirectToPage("/UserTimeline", new { author = author.UserName, page = 1 });
     }
 }
