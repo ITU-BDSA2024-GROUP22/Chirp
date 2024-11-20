17d3a6d0947f145abeb3051b3c6d097821de5968
Stina <stik@itu.dk>
2024-11-20 19:03:43 +0100

    you can now see your own cheeps in the 'about me' section. Still missing the part where the profile details should be displayed

--- src/Chirp.Web/Pages/AboutMe.cshtml.cs
+++ src/Chirp.Web/Pages/AboutMe.cshtml.cs
@@ -1,4 +1,5 @@
 using Chirp.Core.DTOs;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace Chirp.Web.Pages;
@@ -16,7 +17,33 @@ public class AboutMeModel : PageModel
     public required Task<List<CheepDTO>> Cheeps { get; set; }
     public required Task<AuthorDTO> Author { get; set; }
     public int CurrentPage { get; set; }
+    [BindProperty]
+    public string Text { get; set; }
 
+    public ActionResult OnGet([FromQuery] int? page, string author)
+    {
+        this.Author = _service.GetAuthorByName(author);
 
+        CurrentPage = page ?? 1;
+        Cheeps = _service.GetCheepsFromAuthor(author, CurrentPage);
+        return Page();
+    }
+
+    public async Task<IActionResult> OnPostAsync()
+    {
+        if (!User.Identity.IsAuthenticated)
+        {
+            return NotFound();
+        }
+
+        var authorName = User.Identity.Name;
+
+        var author = await _service.GetAuthorByName(authorName);
+
+        await _service.CreateCheep(author, Text, DateTime.UtcNow);
+
+        return RedirectToPage("/UserTimeline", new { author = author.UserName, page = 1 });
+    }
 }
+
 
