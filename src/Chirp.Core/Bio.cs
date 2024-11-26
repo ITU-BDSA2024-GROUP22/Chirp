9436cd6dede0d85d56156ef9a7f6db43e18e7b7c
AVNBuelow <alby@itu.dk>
2024-11-26 15:56:51 +0100

    Bio migrations added succesfully
    Co-authored-by: Anna <anyp@itu.dk>
    Co-authored-by: Stina <stik@itu.dk>

--- src/Chirp.Core/Bio.cs
+++ src/Chirp.Core/Bio.cs
@@ -4,6 +4,8 @@ namespace Chirp.Core;
 
 public class Bio
 {
+    public int BioId { get; set; }
+
     public string? AuthorId { get; set; }
 
     public required Author Author { get; set; }
---
+++ src/Chirp.Web/Migrations/20241126145320_BioEntity.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Chirp.Web.Migrations
+{
+    /// <inheritdoc />
+    public partial class BioEntity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Bios",
+                columns: table => new
+                {
+                    BioId = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    AuthorId = table.Column<string>(type: "TEXT", nullable: true),
+                    Text = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Bios", x => x.BioId);
+                    table.ForeignKey(
+                        name: "FK_Bios_AspNetUsers_AuthorId",
+                        column: x => x.AuthorId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Bios_AuthorId",
+                table: "Bios",
+                column: "AuthorId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Bios");
+        }
+    }
+}
