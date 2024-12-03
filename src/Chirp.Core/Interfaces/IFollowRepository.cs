c7e4f116f80e96e82e5d76d555a87fa5f8e4b71e
AVNBuelow <alby@itu.dk>
2024-12-03 13:23:49 +0100

    FollowRepo is now functional and system works

--- src/Chirp.Core/Interfaces/IFollowRepository.cs
+++ src/Chirp.Core/Interfaces/IFollowRepository.cs
@@ -1,6 +1,11 @@
+using Chirp.Core.DTOs;
+
 namespace Chirp.Core.Interfaces;
 
-public class IFollowRepository
+public interface IFollowRepository
 {
-    
+    public Task FollowAuthor(string followerId, string followeeId);
+    public Task UnfollowAuthor(string followerId, string followeeId);
+    public Task <bool> IsFollowing(string followerId, string followeeId);
+    public List<AuthorDTO> GetFollowing(string user);
 }
