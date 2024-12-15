439ae80c9fe4f6874dcb6908cd98d11bda668db8
AVNBuelow <alby@itu.dk>
2024-12-15 17:51:11 +0100

    back to original

--- test/Chirp.Tests/IntergrationTests.cs
+++ test/Chirp.Tests/IntergrationTests.cs
@@ -12,42 +12,7 @@ namespace Chirp.Tests;
 
 public class IntegrationTests : IClassFixture<WebApplicationFactory<Program>>
 {
-    private readonly WebApplicationFactory<Program> _fixture;
-    private readonly HttpClient _client;
 
-    public IntegrationTests(WebApplicationFactory<Program> fixture)
-    {
-        _fixture = fixture;
-        _client = _fixture.CreateClient(new WebApplicationFactoryClientOptions { AllowAutoRedirect = true, HandleCookies = true });
-    }
-
-    [Fact]
-    public async void CanSeePublicTimeline()
-    {
-        var response = await _client.GetAsync("/");
-        response.EnsureSuccessStatusCode();
-        var content = await response.Content.ReadAsStringAsync();
-
-        Assert.Contains("Chirp!", content);
-        Assert.Contains("Public Timeline", content);
-    }
-
-    [Theory]
-    [InlineData("Helge")]
-    [InlineData("Adrian")]
-    public async void CanSeePrivateTimeline(string author)
-    {
-        var response = await _client.GetAsync($"/{author}");
-        response.EnsureSuccessStatusCode();
-        var content = await response.Content.ReadAsStringAsync();
-
-        Assert.Contains("Chirp!", content);
-        Assert.Contains($"{author}'s Timeline", content);
-    }
-
-
-
-/*
     private readonly WebApplicationFactory<Program> _factory;
     private readonly HttpClient _client;
 
@@ -314,5 +279,4 @@ private void SeedTestData(DBContext context)
         var response = await _client.GetAsync("/Earl/about-me");
         Assert.Equal(HttpStatusCode.OK, response.StatusCode);
     }
-    */
 }
