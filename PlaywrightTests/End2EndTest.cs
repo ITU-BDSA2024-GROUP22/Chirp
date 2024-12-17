c99de419ce635a3b60d1994d7ca295299baaa3c3
Olivia-Brophy <brop@itu.dk>
2024-12-17 21:06:13 +0100

    final e2e testsgit add .

--- PlaywrightTests/End2EndTest.cs
+++ PlaywrightTests/End2EndTest.cs
@@ -1,6 +1,7 @@
 using Microsoft.Playwright;
 using Microsoft.Playwright.NUnit;
 using NUnit.Framework;
+using System;
 using System.Threading.Tasks;
 
 namespace PlaywrightTests
@@ -11,7 +12,7 @@ namespace PlaywrightTests
         private IPlaywright _playwright;
         private IBrowser _browser;
         private IPage _page;
-		private string _randomEmail;
+        private string _randomEmail;
         private string _randomUser;
 
         [SetUp]
@@ -21,10 +22,11 @@ namespace PlaywrightTests
             _browser = await _playwright.Chromium.LaunchAsync(new BrowserTypeLaunchOptions
             {
                 Headless = false,
+                SlowMo = 150,
             });
             var context = await _browser.NewContextAsync();
             _page = await context.NewPageAsync();
-			_randomEmail = GenerateRandomEmail();
+            _randomEmail = GenerateRandomEmail();
             _randomUser = GenerateRandomUsername();
         }
 
@@ -44,20 +46,23 @@ namespace PlaywrightTests
             await LoginUser();
             await PostMessage();
             await NavigateTimelines();
+            await UpdateProfile();
+            await PostAnotherMessage();
+            await ChangeProfilePicture();
             await LogoutUser();
         }
 
-		private string GenerateRandomEmail()
-		{
-    	var random = new Random();
-    	var randomNumber = random.Next(1000, 9999); // Generates a random number between 1000 and 9999
-    	return $"testuser{randomNumber}@example.com";
-		}
+        private string GenerateRandomEmail()
+        {
+            var random = new Random();
+            var randomNumber = random.Next(1000, 9999);
+            return $"testuser{randomNumber}@example.com";
+        }
 
         private string GenerateRandomUsername()
         {
             var random = new Random();
-            var randomNumber = random.Next(1000, 9999); // Generates a random number between 1000 and 9999
+            var randomNumber = random.Next(1000, 9999);
             return $"testuser{randomNumber}";
         }
 
@@ -65,11 +70,9 @@ namespace PlaywrightTests
         {
             await _page.GetByRole(AriaRole.Link, new() { Name = "register" }).ClickAsync();
             await _page.GetByPlaceholder("Name", new() { Exact = true }).FillAsync(_randomUser);
-            await _page.GetByPlaceholder("name@example.com").FillAsync("bole");
-            await _page.GetByPlaceholder("name@example.com").PressAsync("Alt+@");
             await _page.GetByPlaceholder("name@example.com").FillAsync(_randomEmail);
-            await _page.GetByLabel("Password", new() { Exact = true }).FillAsync("Letme111n!");
-            await _page.GetByLabel("Confirm Password").FillAsync("Letme111n!");
+            await _page.GetByPlaceholder("Password", new() { Exact = true }).FillAsync("Letme111n!");
+            await _page.GetByPlaceholder("Confirm Password").FillAsync("Letme111n!");
             await _page.GetByRole(AriaRole.Button, new() { Name = "Register" }).ClickAsync();
         }
 
@@ -88,8 +91,7 @@ namespace PlaywrightTests
 
         private async Task PostMessage()
         {
-            await _page.Locator("#cheepText").ClickAsync();
-            await _page.Locator("#cheepText").FillAsync("Yay for playwright!!");
+            await _page.GetByPlaceholder("Write your cheep here...").FillAsync("Yay for playwright!");
             await _page.GetByRole(AriaRole.Button, new() { Name = "Share" }).ClickAsync();
         }
 
@@ -97,13 +99,35 @@ namespace PlaywrightTests
         {
             await _page.GetByRole(AriaRole.Link, new() { Name = "public timeline" }).ClickAsync();
             await _page.GetByRole(AriaRole.Link, new() { Name = "my timeline" }).ClickAsync();
-            await _page.GetByRole(AriaRole.Link, new() { Name = "public timeline" }).ClickAsync();
+        }
+
+        private async Task UpdateProfile()
+        {
+            await _page.GetByRole(AriaRole.Link, new() { Name = "about me" }).ClickAsync();
+            await _page.GetByLabel("Upload Profile Picture:").SetInputFilesAsync(new[] { "TestFiles/peach.png" });
+            await _page.GetByRole(AriaRole.Button, new() { Name = "Upload" }).ClickAsync();
+            await _page.GetByPlaceholder("Write your bio here...").FillAsync("It's PEACH time!");
+            await _page.GetByRole(AriaRole.Button, new() { Name = "Save Bio" }).ClickAsync();
+        }
+
+        private async Task PostAnotherMessage()
+        {
+            await _page.GetByRole(AriaRole.Link, new() { Name = "my timeline" }).ClickAsync();
+            await _page.GetByPlaceholder("Write your cheep here...").FillAsync("Another cheep!");
+            await _page.GetByRole(AriaRole.Button, new() { Name = "Share" }).ClickAsync();
+
+        }
+
+        private async Task ChangeProfilePicture()
+        {
+            await _page.GetByRole(AriaRole.Link, new() { Name = "about me" }).ClickAsync();
+            await _page.GetByLabel("Change Profile Picture:").SetInputFilesAsync(new[] { "TestFiles/luigi.png" });
+            await _page.GetByRole(AriaRole.Button, new() { Name = "Upload" }).ClickAsync();
         }
 
         private async Task LogoutUser()
         {
-
-            await _page.GetByRole(AriaRole.Link, new() { Name = $"Logout [{_randomUser}]" }).ClickAsync();
+            await _page.GetByRole(AriaRole.Link, new() { Name = $"logout [{_randomUser}]" }).ClickAsync();
             await _page.GetByRole(AriaRole.Button, new() { Name = "Click here to Logout" }).ClickAsync();
         }
     }
